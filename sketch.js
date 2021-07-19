bdb743afdcc5f3af6f9e474ddc20b56254a65917
IRFAN12345862 <84142058+IRFAN12345862@users.noreply.github.com>
2021-07-19 21:04:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var  database;
+var player,form,game;
+var gameState=0;
+var playerCount;
+
+
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+
+  
+}
+
+function draw(){
+
+  
+}
